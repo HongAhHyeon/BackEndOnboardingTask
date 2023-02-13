eef962223e8f46deac88e14ed2898f2f8fe6eff8
HongAhHyeon <ahhyeon.hong@readnumber.com>
2023-02-13 11:59:47 +0900

    initial commit

---
+++ src/test/kotlin/com/onboarding/task/TaskApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.onboarding.task
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TaskApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
